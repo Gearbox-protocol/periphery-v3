b0bd455b983d53c81e2b6f90013d33bcfc3f2989
Mikael <26343374+0xmikko@users.noreply.github.com>
2024-02-25 22:32:39 +0100

    feat: add unpauseAll for MultiPause contract

--- contracts/emergency/MultiPause.sol
+++ contracts/emergency/MultiPause.sol
@@ -9,6 +9,11 @@ import {ICreditManagerV3} from "@gearbox-protocol/core-v3/contracts/interfaces/I
 import {ACLNonReentrantTrait} from "@gearbox-protocol/core-v3/contracts/traits/ACLNonReentrantTrait.sol";
 import {ContractsRegisterTrait} from "@gearbox-protocol/core-v3/contracts/traits/ContractsRegisterTrait.sol";
 
+enum PausableAction {
+    Pause,
+    Unpause
+}
+
 /// @title MultiPause
 /// @author Gearbox Foundation
 /// @notice Allows pausable admins to pause multiple contracts in a single transaction
@@ -23,14 +28,14 @@ contract MultiPause is ACLNonReentrantTrait, ContractsRegisterTrait {
     /// @dev Ignores contracts that are already paused
     /// @dev Reverts if caller is not a pausable admin
     function pauseContracts(address[] memory contracts) external pausableAdminsOnly {
-        _pauseContracts(contracts);
+        _pauseContracts(contracts, PausableAction.Pause);
     }
 
     /// @notice Pauses all registered pools
     /// @dev Ignores contracts that are already paused
     /// @dev Reverts if caller is not a pausable admin
     function pauseAllPools() external pausableAdminsOnly {
-        _pauseAllPools();
+        _pauseAllPools(PausableAction.Pause);
     }
 
     /// @notice Pauses all registered credit managers
@@ -38,24 +43,32 @@ contract MultiPause is ACLNonReentrantTrait, ContractsRegisterTrait {
     /// @dev Ignores contracts that are already paused
     /// @dev Reverts if caller is not a pausable admin
     function pauseAllCreditManagers() external pausableAdminsOnly {
-        _pauseAllCreditManagers();
+        _pauseAllCreditManagers(PausableAction.Pause);
     }
 
     /// @notice Pauses all registered credit managers and pools
     /// @dev Ignores contracts that are already paused
     /// @dev Reverts if caller is not a pausable admin
     function pauseAllContracts() external pausableAdminsOnly {
-        _pauseAllPools();
-        _pauseAllCreditManagers();
+        _pauseAllPools(PausableAction.Pause);
+        _pauseAllCreditManagers(PausableAction.Pause);
+    }
+
+    /// @notice Unpauses all registered credit managers and pools
+    /// @dev Ignores contracts that aren't paused
+    /// @dev Reverts if caller is not a unpausable admin
+    function unpauseAllContracts() external unpausableAdminsOnly {
+        _pauseAllPools(PausableAction.Unpause);
+        _pauseAllCreditManagers(PausableAction.Unpause);
     }
 
     /// @dev Internal function to pause all pools
-    function _pauseAllPools() internal {
-        _pauseContracts(IContractsRegister(contractsRegister).getPools());
+    function _pauseAllPools(PausableAction action) internal {
+        _pauseContracts(IContractsRegister(contractsRegister).getPools(), action);
     }
 
     /// @dev Internal function to pause all credit managers
-    function _pauseAllCreditManagers() internal {
+    function _pauseAllCreditManagers(PausableAction action) internal {
         address[] memory contracts = IContractsRegister(contractsRegister).getCreditManagers();
         uint256 len = contracts.length;
         unchecked {
@@ -64,16 +77,21 @@ contract MultiPause is ACLNonReentrantTrait, ContractsRegisterTrait {
                 contracts[i] = ICreditManagerV3(contracts[i]).creditFacade();
             }
         }
-        _pauseContracts(contracts);
+        _pauseContracts(contracts, action);
     }
 
-    /// @dev Internal function to pause contracts from the given list
-    function _pauseContracts(address[] memory contracts) internal {
+    /// @dev Internal function to pause/unpause contracts from the given list
+    function _pauseContracts(address[] memory contracts, PausableAction action) internal {
         uint256 len = contracts.length;
         unchecked {
             for (uint256 i; i < len; ++i) {
-                if (ACLNonReentrantTrait(contracts[i]).paused()) continue;
-                ACLNonReentrantTrait(contracts[i]).pause();
+                if (action == PausableAction.Pause) {
+                    if (ACLNonReentrantTrait(contracts[i]).paused()) continue;
+                    ACLNonReentrantTrait(contracts[i]).pause();
+                } else {
+                    if (!ACLNonReentrantTrait(contracts[i]).paused()) continue;
+                    ACLNonReentrantTrait(contracts[i]).unpause();
+                }
             }
         }
     }
