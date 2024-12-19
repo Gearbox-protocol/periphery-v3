3393dfefb8eb651405d899d1e55ae6338408fff9
doomsower <12031673+doomsower@users.noreply.github.com>
2024-12-19 14:57:59 -0300

    fix: pendle serializer

--- contracts/serializers/oracles/PendleTWAPPTPriceFeedSerializer.sol
+++ contracts/serializers/oracles/PendleTWAPPTPriceFeedSerializer.sol
@@ -10,6 +10,15 @@ contract PendleTWAPPTPriceFeedSerializer is IStateSerializerLegacy {
     function serialize(address priceFeed) external view override returns (bytes memory) {
         PendleTWAPPTPriceFeed pf = PendleTWAPPTPriceFeed(priceFeed);
 
-        return abi.encode(pf.market(), pf.sy(), pf.yt(), pf.expiry(), pf.twapWindow(), pf.priceToSy());
+        // some already deployed price feeds like 0x9A39068aba808c15A9aAAf04fb58cC6B0E734DC1
+        // don't have priceToSy() function
+        bool priceToSy;
+        try pf.priceToSy() returns (bool _priceToSy) {
+            priceToSy = _priceToSy;
+        } catch {
+            priceToSy = false;
+        }
+
+        return abi.encode(pf.market(), pf.sy(), pf.yt(), pf.expiry(), pf.twapWindow(), priceToSy);
     }
 }
