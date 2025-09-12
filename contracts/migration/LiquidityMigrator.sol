60748d9980ede191d33634297977b1e809108737
Van0k <ivkamakin@gmail.com>
2025-09-12 08:03:12 +0400

    fix: minor audit fixes

--- contracts/migration/LiquidityMigrator.sol
+++ contracts/migration/LiquidityMigrator.sol
@@ -6,12 +6,17 @@ pragma solidity ^0.8.23;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC4626} from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 /// @title LiquidityMigrator
 /// @notice Allows the instance owner to migrate a pool position of a user from one pool to another. The user gives an approval of
 ///         their pool shares to this contract. The instance owner can then migrate assets from specfic `poolFrom` to specific `poolTo`
 ///         at their discretion.
 contract LiquidityMigrator is Ownable {
+    using SafeERC20 for IERC20;
+
+    error PoolAssetsDoNotMatchException();
+
     event Migrate(address indexed user, uint256 assetsAmount);
 
     /// @notice The pool the assets are being migrated from
@@ -29,15 +34,21 @@ contract LiquidityMigrator is Ownable {
         poolFrom = _poolFrom;
         poolTo = _poolTo;
 
-        address asset = IERC4626(poolTo).asset();
-        IERC20(asset).approve(poolTo, type(uint256).max);
+        address assetTo = IERC4626(poolTo).asset();
+        address assetFrom = IERC4626(poolFrom).asset();
+
+        if (assetTo != assetFrom) {
+            revert PoolAssetsDoNotMatchException();
+        }
+
+        IERC20(assetTo).forceApprove(poolTo, type(uint256).max);
     }
 
     /// @notice Migrates assets from `poolFrom` to `poolTo` for `user`
     /// @param user The address of the user whose assets are being migrated
     /// @param amount The amount of `poolFrom` LP shares to migrate
     function migrate(address user, uint256 amount) external onlyOwner {
-        // Shares are redeemedfrom `poolFrom`, which returns the amount of assets withdrawn
+        // Shares are redeemed from `poolFrom`, which returns the amount of assets withdrawn
         uint256 assetsAmount = IERC4626(poolFrom).redeem(amount, address(this), user);
         // The same amount of assets is deposited into `poolTo`, with the user as the receiver of new LP shares
         IERC4626(poolTo).deposit(assetsAmount, user);
