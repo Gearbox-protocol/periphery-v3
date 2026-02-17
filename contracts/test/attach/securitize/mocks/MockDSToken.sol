df63f75f0ec57ae81f92ed37c1709d973de741ef
Dima Lekhovitsky <lekhovitsky@gmail.com>
2026-02-17 17:27:35 +0200

    test: Securitize attach test setup for default liquidity model

---
+++ contracts/test/attach/securitize/mocks/MockDSToken.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockDSToken is ERC20 {
+    address public immutable admin;
+    mapping(address registrar => bool) public isRegistrar;
+    mapping(address investor => bool) public isAuthorized;
+    mapping(address vault => address investor) public registeredVaults;
+
+    error CallerIsNotAdmin(address caller);
+    error CallerIsNotRegistrar(address caller);
+    error InvestorAlreadyAuthorized(address investor);
+    error InvestorNotAuthorized(address investor);
+    error VaultNotRegisteredForInvestor(address vault, address investor);
+    error CannotTransfer(address from, address to);
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) revert CallerIsNotAdmin(msg.sender);
+        _;
+    }
+
+    modifier onlyRegistrar() {
+        if (!isRegistrar[msg.sender]) revert CallerIsNotRegistrar(msg.sender);
+        _;
+    }
+
+    constructor(address admin_) ERC20("Mock DSToken", "MDSToken") {
+        admin = admin_;
+    }
+
+    function setRegistrar(address retistrar, bool status) external onlyAdmin {
+        isRegistrar[retistrar] = status;
+    }
+
+    function mint(address to, uint256 amount) external onlyAdmin {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) external onlyAdmin {
+        _burn(from, amount);
+    }
+
+    function authorize(address investor) external onlyAdmin {
+        if (isAuthorized[investor]) revert InvestorAlreadyAuthorized(investor);
+        isAuthorized[investor] = true;
+    }
+
+    function unauthorize(address investor) external onlyAdmin {
+        if (!isAuthorized[investor]) revert InvestorNotAuthorized(investor);
+        isAuthorized[investor] = false;
+    }
+
+    function registerVault(address vault, address investor) external onlyRegistrar {
+        if (!isAuthorized[investor]) revert InvestorNotAuthorized(investor);
+        registeredVaults[vault] = investor;
+    }
+
+    function unregisterVault(address vault, address investor) external onlyRegistrar {
+        if (registeredVaults[vault] != investor) revert VaultNotRegisteredForInvestor(vault, investor);
+        delete registeredVaults[vault];
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256) internal view override {
+        if (from != address(0) && !_canTransfer(from) || to != address(0) && !_canTransfer(to)) {
+            revert CannotTransfer(from, to);
+        }
+    }
+
+    function _canTransfer(address account) internal view returns (bool) {
+        return isAuthorized[account] || isAuthorized[registeredVaults[account]];
+    }
+}
---
+++ contracts/test/attach/securitize/mocks/MockRegistrar.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {IVaultRegistrar} from "../../../../securitize/interfaces/IVaultRegistrar.sol";
+import {MockDSToken} from "./MockDSToken.sol";
+
+contract MockRegistrar is IVaultRegistrar {
+    address public immutable admin;
+    address public immutable override token;
+
+    mapping(address => bool) public isOperator;
+
+    error CallerIsNotAdmin(address caller);
+    error CallerIsNotOperator(address caller);
+    error InvestorNotRegisteredInToken(address investor);
+    error VaultNotRegistered(address vault, address investor);
+
+    modifier onlyAdmin() {
+        if (msg.sender != admin) revert CallerIsNotAdmin(msg.sender);
+        _;
+    }
+
+    modifier onlyOperator() {
+        if (!isOperator[msg.sender]) revert CallerIsNotOperator(msg.sender);
+        _;
+    }
+
+    constructor(address admin_, address token_) {
+        admin = admin_;
+        token = token_;
+    }
+
+    function grantOperator(address account) external onlyAdmin {
+        isOperator[account] = true;
+    }
+
+    function isRegistered(address vaultAddress, address investorWalletAddress) external view override returns (bool) {
+        return MockDSToken(token).registeredVaults(vaultAddress) == investorWalletAddress
+            && MockDSToken(token).isAuthorized(investorWalletAddress);
+    }
+
+    function registerVault(address vaultAddress, address investorWalletAddress) external override onlyOperator {
+        MockDSToken(token).registerVault(vaultAddress, investorWalletAddress);
+    }
+
+    function unregisterVault(address vaultAddress, address investorWalletAddress) external override onlyOperator {
+        MockDSToken(token).unregisterVault(vaultAddress, investorWalletAddress);
+    }
+}
